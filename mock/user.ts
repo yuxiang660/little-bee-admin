7e18a78fa81f65ef772f5ccdca1ef75cde874443
yuxiang660 <yuxiang660@163.com>
2020-02-16 13:44:38 +0800

    unify userInfo mock data

--- mock/user.ts
+++ mock/user.ts
@@ -44,22 +44,19 @@ export default {
 
     return res.status(200).send([
       {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
+        name: 'Little Bee Admin',
+        userid: 'admin_uuid',
+        role: 'admin',
       },
       {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
+        name: 'Little Bee User',
+        userid: 'user_uuid',
+        role: 'user',
       },
       {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sidney No. 1 Lake Park',
+        name: 'Little Bee Guest',
+        userid: 'guest_uuid',
+        role: 'guest',
       },
     ]);
   },
